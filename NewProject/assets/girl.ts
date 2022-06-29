a360f8de96b57f0f79394dd200c5774b101edfab
LinuxDev <lin_hong_quan_msn@hotmail.com>
2022-06-29 22:58:37 +0800

    修改动画变量SKETLETON数据

--- NewProject/assets/girl.ts
+++ NewProject/assets/girl.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node,sp, Button, SpriteComponent } from 'cc';
+import { _decorator, Component, Node,sp, Button, SpriteComponent, resources } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('girl')
@@ -17,6 +17,14 @@ export class girl extends Component {
 
     skinId:number=0;
 
+    firstSkinPath:number=0;
+
+    isBoySkin:boolean=false;
+
+    aniname:string[] = ['dance','walk'];
+
+    skinpath:string[]=["girl/mix-and-match-pro","spineboy/spineboy-pro"];
+
     start() {
 
     }
@@ -27,11 +35,11 @@ export class girl extends Component {
 
     //播放动画
     playAnimation(){
-        this.SkeObj.setAnimation(0,"dance",true);
+        this.SkeObj.setAnimation(0,this.aniname[this.firstSkinPath],true);
     }
     //停止动画
     stopAnimation(){
-       let SpEnt:sp.spine.TrackEntry =  this.SkeObj.setAnimation(0,"dance",true);
+       let SpEnt:sp.spine.TrackEntry =  this.SkeObj.setAnimation(0,this.aniname[this.firstSkinPath],true);
        SpEnt.animationStart = SpEnt.animationEnd;
     }
     //换肤
@@ -39,6 +47,17 @@ export class girl extends Component {
         const skins =['girl', 'boy', 'girl-blue-cape', 'girl-spring-dress'].map(x=> `full-skins/${x}`);
         this.skinId = (this.skinId + 1) % skins.length;
         this.SkeObj!.setSkin(skins[this.skinId]);
+    }
+
+    changeSkeletonData(){
+        this.firstSkinPath = (this.firstSkinPath + 1) % this.skinpath.length;
+        resources.load(this.skinpath[this.firstSkinPath],sp.SkeletonData,(err,skedata)=>{
+            this.SkeObj.skeletonData = skedata;
+            if(this.aniname[this.firstSkinPath]=='dance'){
+                this.SkeObj.setSkin('full-skins/girl-spring-dress');
+            }
+            this.SkeObj.setAnimation(0,this.aniname[this.firstSkinPath],true);
+        });
     }
 }
 
